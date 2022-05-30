83323bb5b7cfee043dc2be8baa6da6c567e81408
IrmaLazauskiene <103452303+IrmaLazauskiene@users.noreply.github.com>
2022-05-30 13:21:35 +0300

    Add files via upload

--- meniu.js
+++ meniu.js
@@ -1,25 +1,25 @@
 
 var text = '{"pavadinimai":[' +
-'{"Name":"Špinatai","gr":160,"ang":5.6,"blt":4.48,"rbl":0.48},' +
-'{"Name":"Grietinė 30%","gr":20,"ang":0.62,"blt":0.38,"rbl":6},' +
-'{"Name":"Virti grikiai","gr":160,"ang":31.84,"blt":5.28,"rbl":0.98},' +
-'{"Name":"Braškės","gr":60,"ang":5.82,"blt":0.54,"rbl":0.24},' +
-'{"Name":"Avokado aliejus","gr":15,"ang":0,"blt":0,"rbl":15 },' +
-'{"Name":"Pomidorai agurkai","gr":300,"ang":11.7,"blt":3,"rbl":0.6},' +
-'{"Name":"Išrūgos","gr":150,"ang":7.5,"blt":1.5,"rbl":0},' +
-'{"Name":"Lašiša","gr":70,"ang":0,"blt":9.03,"rbl":9.52},' +
-'{"Name":"Obuoliai kriaušės","gr":200,"ang":25,"blt":0.7,"rbl":0.4},' +
-'{"Name":"Alyvuogių aliejus","gr":10,"ang":0,"blt":0,"rbl":10 },' +
-'{"Name":"Salotos ridikėliai","gr":200,"ang":5.1,"blt":2.2,"rbl":0},' +
-'{"Name":"Jogurtas","gr":50,"ang":6.2,"blt":1.45,"rbl":0.4},' +
-'{"Name":"Avižinė košė su Chia","gr":80,"ang":44.8,"blt":11.2,"rbl":7.6},' +
-'{"Name":"Riešutų mišinys","gr":30,"ang":5.4,"blt":6,"rbl":17.1},' +
-'{"Name":"Ryžių aliejus","gr":30,"ang":0,"blt":0,"rbl":6},' +
-'{"Name":"Morkos saulėgrąžos","gr":110,"ang":12.6,"blt":3.3,"rbl":5.9},' +
+'{"Name":"Špinatai","gr":160,"ang":6.3,"blt":5,"rbl":0.48},' +
+'{"Name":"Grietinė 30%","gr":20,"ang":0.62,"blt":0.82,"rbl":6},' +
+'{"Name":"Virti grikiai","gr":170,"ang":35.5,"blt":5.8,"rbl":1},' +
+'{"Name":"Braškės","gr":70,"ang":6.5,"blt":0.84,"rbl":0.3},' +
+'{"Name":"Avokado aliejus","gr":5,"ang":0,"blt":0,"rbl":5 },' +
+'{"Name":"Pomidorai agurkai","gr":150,"ang":8.8,"blt":1.5,"rbl":0.1},' +
+'{"Name":"Išrūgos","gr":160,"ang":11.5,"blt":1.5,"rbl":0},' +
+'{"Name":"Lašiša","gr":70,"ang":0,"blt":8.63,"rbl":8.5},' +
+'{"Name":"Obuoliai, kriaušės","gr":100,"ang":19.8,"blt":0.35,"rbl":0.1},' +
+'{"Name":"Alyvuogių aliejus","gr":2,"ang":0,"blt":0,"rbl":2 },' +
+'{"Name":"Salotos, ridikėliai","gr":200,"ang":5.8,"blt":2.2,"rbl":0},' +
+'{"Name":"Jogurtas","gr":50,"ang":6.8,"blt":1.45,"rbl":0.4},' +
+'{"Name":"Avižinė košė su Chia","gr":80,"ang":46.8,"blt":11.2,"rbl":6.5},' +
+'{"Name":"Riešutų mišinys","gr":15,"ang":3,"blt":3,"rbl":7.5},' +
+'{"Name":"Ryžių aliejus","gr":10,"ang":0,"blt":0,"rbl":2},' +
+'{"Name":"Morkos, saulėgrąžos (100/10)","gr":110,"ang":12.6,"blt":3,"rbl":6.2},' +
 '{"Name":"Makaronai","gr":50,"ang":37,"blt":6,"rbl":1},' +
-'{"Name":"Menkų filė","gr":50,"ang":0,"blt":7.55,"rbl":0.4},' +
+'{"Name":"Menkų filė","gr":50,"ang":0,"blt":6.55,"rbl":0.7},' +
 '{"Name":"Vynuogės","gr":50,"ang":9,"blt":0.35,"rbl":0.1},' +
-'{"Name":"Linų sėmenų aliejus","gr":25,"ang":0,"blt":0,"rbl":25}]}';
+'{"Name":"Linų sėmenų aliejus","gr":5,"ang":0,"blt":0,"rbl":5}]}';
 
 function dalis (kalorijos, procentai)
 {
@@ -49,7 +49,8 @@ const rasti= function(kalorijos)
       var aviso = 0;
       var bviso = 0;
       var rviso = 0;
-      maistas += meniu + "-as meniu dienai: " + "<br>";
+      maistas +="<div class='responsive'><div class='gallery'>";
+      maistas +=meniu + "-as meniu dienai: " + "<br><br>";
       for (let j = i; j < i+5; j++) {
         viso += obj.pavadinimai[j].blt*4 + obj.pavadinimai[j].rbl*9 + obj.pavadinimai[j].ang*4;
         }
@@ -62,7 +63,7 @@ const rasti= function(kalorijos)
         let gramai = obj.pavadinimai[j].gr*santykis;
         maistas +="- " + obj.pavadinimai[j].Name + " " + parseInt(gramai) + "g<br>";
       }
-    maistas += meniu + "-ame meniu yra: baltymų " + parseInt(bviso*4) + "g, riebalų " + parseInt(rviso*9) + "g, angliavandenių " + parseInt(aviso*4) +"g.<br><br>";
+    maistas += "<br>" + meniu + "-ame meniu yra: baltymų " + parseInt(bviso*4) + "g, riebalų " + parseInt(rviso*9) + "g, angliavandenių " + parseInt(aviso*4) +"g.<br></div></div>";
   }
 }
   return maistas;
